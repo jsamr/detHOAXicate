e8c584966e6e3a5eecb38b3393ddc2d64dd0fe0e
sveinburne <theswanted@gmail.com>
2016-08-27 04:12:26 -0400

    fixed eslint

--- server/init-app.js
+++ server/init-app.js
@@ -2,7 +2,7 @@ import path from 'path'
 import express from 'express'
 import webpack from 'webpack'
 import webpackMiddleware from 'webpack-dev-middleware'
-import webpackHotMiddleware from'webpack-hot-middleware'
+import webpackHotMiddleware from 'webpack-hot-middleware'
 import config from '../webpack.config'
 import articleConfig from '../webpack.article.config'
 
