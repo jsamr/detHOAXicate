d992d46d3da630062a80b1c7bec2b1843383ac7b
sveinburne <theswanted@gmail.com>
2016-08-31 18:16:47 -0400

    improve security
    - prevent any extenal website to include this app in an iframe

--- server/init-security.js
+++ server/init-security.js
@@ -2,7 +2,10 @@
 // import cors from 'cors'
 
 function setContentPolicy (req, res, next) {
+  // prevent external scripts
   res.setHeader('Content-Security-Policy', "script-src 'self'")
+  // prevent this app to be embedded in an external iFrame
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN')
   next()
 }
 
